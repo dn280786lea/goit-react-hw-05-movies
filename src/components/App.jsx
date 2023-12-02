fc3fd1ae7075e801a4abe2a47c4e3d20659564e9
dn280786lea <ekaterina.lubenets@gmail.com>
2023-12-02 15:48:00 +0100

    header
    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import MovieDetails from './MovieDetails/MovieDatails';
+import Title from './Title/Title';
 
 const App = () => {
   return (
     <>
+      <Title />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {} from './Title.css';
 
 const Title = () => {
-  return <h1>Title Component</h1>;
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="active" exact>
+        Home
+      </NavLink>
+      <NavLink to="/movies" activeClassName="active">
+        Movies
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Title;
